51bf948ca3b9e39ee21d99ef92aa741bd2cc96b0
ktotheH <kcjhill1234@msn.com>
2020-08-26 13:29:52 -0600

    service worker

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# OnlineOfflineBudgetTrackers
+
+  ![badmath](https://img.shields.io/github/last-commit/kcjhill1234/OnlineOfflineBudgetTrackers) ![badmath](https://img.shields.io/github/issues-pr/kcjhill1234/OnlineOfflineBudgetTrackers) ![badmath](https://img.shields.io/github/languages/top/kcjhill1234/OnlineOfflineBudgetTrackers)
+# Online Offline Budget Tracker (PWA)
+
+## Description
+
+This is a Budget Tracker that uses improvements associated with Progressive Web App development (PWA) in order to make it more efficient online and offline. 
+
+
+## contributing
+
+Kenny Hill
+
---
+++ public/service-worker.js
@@ -0,0 +1,50 @@
+console.log("Hello from service worker Plus!")
+
+const FILES_TO_CACHE = [
+    "/",
+    "/models/transaction.js",
+    "/public/icons/icon-192x192.png",
+    "/public/icons/icon-512x512.png",
+    "/public/index.html",
+    "/public/index.js",
+    "/public/service-worker.js",
+    "/public/styles.css",   
+    "/node_modules/",
+    "/routes/api.js",
+    "/package-lock.json",
+    "/package.json"
+];
+
+let CACHE_NAME = "records"; 
+
+self.addEventListener("install", event => {
+    console.log("installing...");
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+            .catch(err => console.log(err))
+    );
+});
+
+// self.addEventListener("fetch", event => {
+//     console.log("You fetched " + event.url);
+// });
+
+self.addEventListener("fetch", event => {
+    if (event.request.url === "http://localhost:3000/") {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                self.cache.open(CACHE_NAME).then(cache => cache.match("/offline.html"))
+            )
+        );
+    } else {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                caches.match(event.request).then(response => response)
+            )
+        );
+    }
+});
