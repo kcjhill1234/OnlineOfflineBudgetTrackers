866e3a116eb181b0ac6eee8fdfa29a16aedc4a9a
ktotheH <kcjhill1234@msn.com>
2020-08-25 22:33:56 -0600

    ugh

---
+++ Develop/routes/index.js
@@ -0,0 +1,12 @@
+// const router = require("express").Router();
+// const path = require("path");
+
+// const api = require("./api.routes");
+
+// router.use("/api", api);
+
+// router.get("/", (req, res) => {
+//     res.sendFile(path.resolve("public", "index.html"))
+// });
+
+// module.exports = router;
